c99d433e45d59d158d75fa34427d0d48ba4816b3
Nermeenhassan1996 <nermeenhassan1996@gmail.com>
2023-12-11 22:24:38 -0800

    all

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Created on Tue May 25 2020
+@author: nermo
+"""
+
+
+class LockedClass:
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ['first_name']
