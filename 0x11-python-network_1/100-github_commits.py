f165f8f3d7ac36a4bf74b3d18bdd8a6ae8a303f7
Nermeenhassan1996 <nermeenhassan1996@gmail.com>
2024-03-05 18:14:52 -0800

    all

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,23 +2,22 @@
 """
 script that takes 2 arguments in order to list 10 commits 
 """
-import sys
-import requests
+from requests import get
+from sys import argv
 
 
 if __name__ == "__main__":
+    username = argv[2]
+    repo = argv[1]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(username, repo)
+    response = get(url)
     try:
-        repo_name = sys.argv[1]
-        username = sys.argv[2]
-        commmits_url = "https://api.github.com/repos/{}/{}/commits" \
-            .format(username, repo_name)
-        response = requests.get(commmits_url)
-        json_obj = response.json()
-        for i, obj in enumerate(json_obj):
-            if i == 10:
+        objects = response.json()
+        for i, obj in enumerate(objects):
+            print('{}: {}'.format(obj.get('sha'),
+                                  obj.get('commit').get('author')
+                                  .get('name')))
+            if i == 9:
                 break
-            if type(obj) is dict:
-                name = obj.get('commit').get('author').get('name')
-                print("{}: {}".format(obj.get('sha'), name))
-    except ValueError as invalid_json:
+    except ValueError:
         pass
